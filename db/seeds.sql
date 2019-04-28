30b5166322e6c1789cb573368471d17847bc554e
bwillstorey <bwillstorey@gmail.com>
2019-04-28 14:39:30 -0500

    initial app setup

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Patty Melt', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hawaiian', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southern', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black & Blue', true);
