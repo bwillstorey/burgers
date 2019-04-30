e39cc60038d3faa5a0b1a32d9af594a3713fc6e4
bwillstorey <bwillstorey@gmail.com>
2019-04-29 20:40:09 -0500

    replacing login with dotenv for db password protection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var login = require("../login");
+
+require("dotenv").config();
 
 var connection;
 
@@ -12,7 +13,7 @@ else {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: login.password,
+    password: process.env.DB_PASS,
     database: "burgers_db"
   });
 }
